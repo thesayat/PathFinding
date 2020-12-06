7facac8dfb8cd601f1b87f91c8edaca8f5c499f2
DSiemienik <dsiemienik@gmail.com>
2020-12-06 22:56:03 +0100

    introduce board controller class to manage map generation

---
+++ PathFinding/Assets/Scripts/Gameplay/BoardController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoardController : MonoBehaviour
+{
+    [Header("Tiles Prefabs")]
+    public GameObject FloorPrefab;
+    public GameObject ObstaclePrefab;
+    public GameObject StartPrefab;
+    public GameObject EndPrefab;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        CreateBoard();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    public void CreateBoard(int size = 10){
+        for(int i = 0; i < size; ++i){
+
+            for(int j = 0; j < size; ++j){
+                Instantiate(FloorPrefab, new Vector3(i, j, 0), Quaternion.identity);
+            }
+        }
+    }
+
+}
